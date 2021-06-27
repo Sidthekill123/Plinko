de9c8db92462d4b0b25cdcc1a5bf824ba37f7d54
Sidthekill123 <75685035+Sidthekill123@users.noreply.github.com>
2021-06-27 13:47:40 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,12 @@
+class Particle {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.r = r;
+        this.color = color(random (0,255),random(0,255),random(0,255));     
+        World.add(world, this.body);
+    }
+}
